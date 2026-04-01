d2ef04e7db7346d7a3537cbbbcf2c327ee545b22
akho0036 <akho0036@student.monash.edu>
2026-04-02 02:06:42 +1100

    Return None, not 0, on invalid input

--- calc.py
+++ calc.py
@@ -1,2 +1,7 @@
 def compute(expression):
-    return None
+    num0, operator, num1 = expression.split(' ')
+    if operator == '+':
+        return num0 + num1
+    else:
+        print('unknown operator!')
+        return None
